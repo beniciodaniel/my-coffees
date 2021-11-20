29c3389e6104ecdba31fc37f744e1f658b3a8f95
Benício Barreto <benicio.daniel@gmail.com>
2021-11-20 17:25:39 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled-Components
+    </S.Description>
+  </S.Wrapper>
+)
+
+export default Main
