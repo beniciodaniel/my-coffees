56c7fd1fa5229a84a16d50555071aab1433e8ae5
Benício Barreto <benicio.daniel@gmail.com>
2021-11-21 10:57:41 -0300

    added graphql-codegen to add types

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,6 +3,7 @@ import PageTemplate, { PageTemplateProps } from 'templates/Pages'
 import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
 import { useRouter } from 'next/router'
 import { GetStaticProps } from 'next'
+import { GetPageBySlugQuery, GetPagesQuery } from 'graphql/generated/graphql'
 
 export default function Page({ heading, body }: PageTemplateProps) {
   const router = useRouter()
@@ -14,7 +15,7 @@ export default function Page({ heading, body }: PageTemplateProps) {
 }
 
 export const getStaticPaths = async () => {
-  const { pages } = await client.request(GET_PAGES, { first: 3 })
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
 
   const paths = pages.map(({ slug }) => ({
     params: { slug }
@@ -24,7 +25,7 @@ export const getStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
     slug: `${params?.slug}`
   })
 
